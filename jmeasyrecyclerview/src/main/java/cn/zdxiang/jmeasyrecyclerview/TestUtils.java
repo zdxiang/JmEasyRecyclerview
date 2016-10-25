3a5f04275e2a6383c4d9338e01e6294c76e897f9
Jm <365421046@qq.com>
2016-10-25 11:18:27 +0800

    Initial commit

---
+++ jmeasyrecyclerview/src/main/java/cn/zdxiang/jmeasyrecyclerview/TestUtils.java
@@ -0,0 +1,18 @@
+package cn.zdxiang.jmeasyrecyclerview;
+
+import android.util.Log;
+
+/**
+ * Created on 16/10/25.
+ *
+ * @author JM
+ * @version v1.0
+ * @discrition TestUtils
+ */
+
+public class TestUtils {
+
+    public static void test() {
+        Log.d("fuckyou", "hhhhh");
+    }
+}
